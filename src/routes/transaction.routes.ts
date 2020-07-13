d0ec4df7f42736d938d9bc0e2ee41e7610eacf8c
Xavi002 <diogomartins200214@gmail.com>
2020-07-13 11:46:38 +0100

    first commit

---
+++ src/routes/transaction.routes.ts
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+const transactionRouter = Router();
+
+const transactionsRepository = new TransactionsRepository();
+
+transactionRouter.get('/', (request, response) => {
+  try {
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+transactionRouter.post('/', (request, response) => {
+  try {
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService( // Trata das regras de negócio
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      // Cria a transação
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default transactionRouter;
