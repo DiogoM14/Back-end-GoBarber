19cfd6af267c4bdd1026729652f5c978cc853c1c
Xavi002 <diogomartins200214@gmail.com>
2020-07-15 10:49:11 +0100

    updating avatar

--- src/models/User.ts
+++ src/models/User.ts
@@ -23,6 +23,9 @@ class User {
   @Column() // Utiliza varchar default
   password: string;
 
+  @Column() // Utiliza varchar default
+  avatar: string;
+
   @CreateDateColumn()
   created_at: Date;
 
---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import { json } from 'express';
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar.');
+    }
+
+    if (user.avatar) {
+      // Apagar avatar anterior
+
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath); // Verifica se encontra um file igual ao userAvatarFilePath
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath); // Se existir apagar o avatar
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
